292589909c1f7a406a899ab747c6dd246fc7e65f
jhecking_10 <jhecking_10@protonmail.com>
2026-09-16 19:35:18 +0900

    Updated constants, main, and player files; added shot.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def game_loop():
@@ -15,7 +16,7 @@ def game_loop():
     dt: float = 0.0 # delta time stores decimal number of seconds
     font = pygame.font.Font(None, 34)
 
-    updatable, drawable, asteroids = group()
+    updatable, drawable, asteroids, shots = group()
     game_screen = create_screen()
     create_asteroid_field()
     player = create_player()
@@ -29,6 +30,7 @@ def game_loop():
                 
         fill_screen(game_screen)
         updatable.update(dt)
+        player.shot_cooldown -= dt
 
         # Check if player collides with any asteroid
         for a in asteroids:
@@ -37,9 +39,11 @@ def game_loop():
                 print("Game over!")
                 sys.exit()
 
-        # Display number of asteroids spawned in
+        # Display number of shots fired and number of asteroids spawned in
+        # Will later use to calculate accuracy percentage
         asteroid_count = len(asteroids)
-        text_surface = font.render(f"Asteroids spawned: {asteroid_count}", True, "white")
+        shots_fired = len(shots)
+        text_surface = font.render(f"Asteroids spawned: {asteroid_count} | Shots fired: {shots_fired}", True, "white")
         game_screen.blit(text_surface, (10, 10))
         
         for object in drawable:
@@ -64,10 +68,12 @@ def group():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroid = pygame.sprite.Group()
+    shot = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroid, updatable, drawable)
     AsteroidField.containers = (updatable)
-    return updatable, drawable, asteroid
+    Shot.containers = (shot, updatable, drawable)
+    return updatable, drawable, asteroid, shot
     
 def draw(object, screen):
     object.draw(screen)
--- player.py
+++ player.py
@@ -1,13 +1,22 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import (
+    LINE_WIDTH,
+    PLAYER_RADIUS,
+    PLAYER_SHOOT_COOLDOWN_SECONDS,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+)
+from shot import Shot
 
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     # in the Player class
     def triangle(self) -> list[pygame.Vector2]:
@@ -20,7 +29,6 @@ class Player(CircleShape):
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
-        # return super().draw(screen)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -34,11 +42,24 @@ class Player(CircleShape):
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_UP]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_DOWN]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        if self.shot_cooldown > 0:
+            return
+        else:
+            self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+            x, y = self.position
+            shot = Shot(x, y)
+            shot.velocity = pygame.Vector2(0, 1)
+            shot.velocity.rotate_ip(self.rotation)
+            shot.velocity *= PLAYER_SHOOT_SPEED
